d3a36e684b541de0e22605bcc2e743e23e050d2f
Magnus Karström <mrkarstrom81@gmail.com>
2025-07-02 17:27:25 +0200

    add: Using interface to make types for props.

--- src/components/Greeter.tsx
+++ src/components/Greeter.tsx
@@ -1,7 +1,17 @@
-import type React from 'react';
+// import type React from 'react';
 
-function Greeter(props: { person: string }): React.JSX.Element {
-  return <h1>Hello! {props.person}</h1>;
+// function Greeter(props: { person: string }): React.JSX.Element {
+//   return <h1>Hello! {props.person}</h1>;
+// }
+
+// export default Greeter;
+
+interface GreeterProps {
+  person: string;
+}
+
+function Greeter({ person }: GreeterProps): React.JSX.Element {
+  return <h1>Hello {person}</h1>;
 }
 
 export default Greeter;
