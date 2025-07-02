b527fde580deea7fc4f6efde854cff6867da3375
Magnus Karström <mrkarstrom81@gmail.com>
2025-07-02 21:36:21 +0200

    add: using newest import type {FC} instead.

---
+++ src/components/ShoppingList.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react';
+
+const ShoppingList: FC = () => {
+  return (
+    <div className="">
+      <h1>Shopping List</h1>
+      <ul>
+        <li>Item1</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ShoppingList;
