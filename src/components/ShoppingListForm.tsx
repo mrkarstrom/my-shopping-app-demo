c0cf977975b44bbb8a2d19472b7969d62fe632e4
Magnus <magnus@yesweb.se>
2025-07-05 19:05:00 +0200

    add: Added the useRef to input.

--- src/components/ShoppingListForm.tsx
+++ src/components/ShoppingListForm.tsx
@@ -1,11 +1,21 @@
+import { useRef } from 'react';
+
 function ShoppingListForm(): React.JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     console.log('Submitted');
+    console.log(inputRef.current?.value);
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="" id="" placeholder="Product Name" />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Product Name"
+        ref={inputRef}
+      />
       <button type="submit">Add</button>
     </form>
   );
